dfcbf1e83ea3ed1e49fc49a4fb6c3b354f42ad7b
jangseokgyu <43026098+jangseokgyu@users.noreply.github.com>
2019-01-08 22:40:07 +0900

    Create data_structure.java

---
+++ data_structure.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int pf = scan.nextInt();
+
+    if(pf == 123)
+      System.out.println("yes you are right");
+    else
+      System.out.println("idiot");
+  }
+}
